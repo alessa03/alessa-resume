2775d821a88f42331e5a85236a2c291629e19a5e
alessa03 <74395036+alessa03@users.noreply.github.com>
2020-11-13 12:03:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function bigImage(x) {
+  x.style.height = "310px";
+  x.style.width = "230px";
+}
+
+function normalImage(x) {
+  x.style.height = "300px";
+  x.style.width = "220px";
+}
+
+alert ("Make sure your website size is full screen or 100%, to see my E-resume better quality!");
